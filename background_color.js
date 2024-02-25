b4e5aad05b3d9f30002ca49b680f2eb447ca7d28
szanyij <szanyij@gmail.com>
2024-02-25 17:35:26 +0100

    1

--- background_color.js
+++ background_color.js
@@ -13,38 +13,35 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-
-// for debugging if the add=on is connected 
-// document.body.style.border = "5px solid red"
-
-//let STATE_KEY = "STATE_KEY"
+let gVersion = "1";
 
 function dark() {
   function returnColors(result) {
-    let ColorsList = result.Colors.split("|");
     try {
-      ltxtColor = ColorsList[0];
-      lbackgroundColor = ColorsList[1];
-      ltestColor = ColorsList[2];
-      ldarkBlueColor = ColorsList[3];
-      lcodeBackground = ColorsList[4];
-      llinkColor = ColorsList[5];
+      let ColorsList = result.Colors.split("|");
+      let lVersion = ColorsList[0];
+      if (gVersion !== lVersion)
+        throw new Error("Version difference -> reset settings.");
+      lBackgroundColor = ColorsList[1];
+      lEnableTextColoring = ColorsList[2];
+      lTxtColor = ColorsList[3];
+      lCodeBackground = ColorsList[4];
+      lLinkColor = ColorsList[5];
     } catch (error) {
-      ltxtColor = "#8F99A5";
-      lbackgroundColor = "#2A2C37";
-      ltestColor = "#1FED18";
-      ldarkBlueColor = "#000022";
-      lcodeBackground = "#1c1c1c";
-      llinkColor = "#687b9a";
+      console.log(error);
+      lBackgroundColor = "#2A2C37";
+      lEnableTextColoring = "1";
+      lTxtColor = "#8F99A5";
+      lCodeBackground = "#1c1c1c";
+      lLinkColor = "#687b9a";
     }
     changeColors(
       {
-        txtColor: ltxtColor,
-        backgroundColor: lbackgroundColor,
-        testColor: ltestColor,
-        darkBlueColor: ldarkBlueColor,
-        codeBackground: lcodeBackground,
-        linkColor: llinkColor
+        backgroundColor: lBackgroundColor,
+        enableTextColoring: lEnableTextColoring,
+        txtColor: lTxtColor,
+        codeBackground: lCodeBackground,
+        linkColor: lLinkColor
       })
   }
   function onError(error) {
@@ -52,32 +49,22 @@ function dark() {
   }
   let Colors = browser.storage.sync.get("Colors");
   Colors.then(returnColors, onError);
-  /*  return {
-     txtColor: browser.storage.sync.get('txtColor') || '#8F99A5',
-     backgroundColor: browser.storage.sync.get('backgroundColor') || "#2A2C37",
-     testColor: browser.storage.sync.get('testColor') || '#1FED18',
-     darkBlueColor: browser.storage.sync.get('darkBlueColor') || '#000022',
-     codeBackground: browser.storage.sync.get('codeBackground') || '#1c1c1c',
-     linkColor: browser.storage.sync.get('linkColor') || '#687b9a'
-   } */
 }
 
 function pink() {
   return {
-    txtColor: '#444',
     backgroundColor: "#ddffff",  // pink
-    testColor: '#1FED18',  // green 
-    darkBlueColor: '#000022',
+    enableTextColoring: "1",
+    txtColor: '#444',
     codeBackground: '#bcbcbc',
     linkColor: '#4b7cf0'
   }
 }
 
 function changeColors(colors) {
-  const txtColor = colors.txtColor
   const backgroundColor = colors.backgroundColor
-  const testColor = colors.testColor
-  const darkBlueColor = colors.darkBlueColor
+  const enableTextColoring = colors.enableTextColoring
+  const txtColor = colors.txtColor
   const codeBackground = colors.codeBackground
   const linkColor = colors.linkColor
 
@@ -97,30 +84,33 @@ function changeColors(colors) {
     }
   }
 
-  colorTextByTagName("p")
-  colorTextByTagName("a", linkColor)
-  colorTextByTagName("li")
-  colorTextByTagName("ul")
-  colorTextByTagName("b")
-  colorTextByTagName("table")
-  colorTextByTagName("th")
-  colorTextByTagName("td")
-  colorTextByTagName("h1")
-  colorTextByTagName("h2")
-  colorTextByTagName("h3")
-  colorTextByTagName("h4")
-  colorTextByTagName("h5")
-  colorTextByTagName("h6")
-  colorTextByTagName("input")
-  colorTextByTagName("input id")
-  colorTextByTagName("div")
-  colorTextByTagName("class")
-  colorTextByClass("result__snippet")
-  colorTextByTagName("span")
-  colorTextByTagName("label")
-  colorTextByTagName("figcaption")
-  colorTextByTagName("pre")
-  colorTextByTagName("code")
+  IF(enableTextColoring === "1")
+  {
+    colorTextByTagName("p")
+    colorTextByTagName("a", linkColor)
+    colorTextByTagName("li")
+    colorTextByTagName("ul")
+    colorTextByTagName("b")
+    colorTextByTagName("table")
+    colorTextByTagName("th")
+    colorTextByTagName("td")
+    colorTextByTagName("h1")
+    colorTextByTagName("h2")
+    colorTextByTagName("h3")
+    colorTextByTagName("h4")
+    colorTextByTagName("h5")
+    colorTextByTagName("h6")
+    colorTextByTagName("input")
+    colorTextByTagName("input id")
+    colorTextByTagName("div")
+    colorTextByTagName("class")
+    colorTextByClass("result__snippet")
+    colorTextByTagName("span")
+    colorTextByTagName("label")
+    colorTextByTagName("figcaption")
+    colorTextByTagName("pre")
+    colorTextByTagName("code")
+  }
 
   function colorBackground(elementType, bgColor = backgroundColor, id) {
     const elements = document.getElementsByTagName(elementType);
@@ -164,7 +154,7 @@ function doIfKey(e) {
     else if (lSTATE === "1") {
       lSTATE = "2"
       browser.storage.sync.set({ STATE_KEY: lSTATE, });
-      document.location.reload();      
+      document.location.reload();
     }
     else if (lSTATE === "2") {
       lSTATE = "0"
--- settings/options.js
+++ settings/options.js
@@ -1,92 +1,62 @@
 /*
-txtColor: '#8F99A5',
-backgroundColor: "#2A2C37",
-testColor: '#1FED18',
-darkBlueColor: '#000022',
-codeBackground: '#1c1c1c',
-linkColor: '#687b9a' 
+    // version: 1 //
+    backgroundColor: "#ddffff",  // pink
+    enableTextColoring: "1",
+    txtColor: '#444',
+    codeBackground: '#bcbcbc',
+    linkColor: '#4b7cf0'
 #D4D4D4
 */
+
+let gVersion = "1";
+
 function saveOptions(e) {
     e.preventDefault();
-    let Colors = document.querySelector("#txtColor").value + '|' +
+    let Colors = document.querySelector("#version").value + '|' +
         document.querySelector("#backgroundColor").value + '|' +
-        document.querySelector("#testColor").value + '|' +
-        document.querySelector("#darkBlueColor").value + '|' +
+        ( document.querySelector("#enableTextColoring").checked ? "1" : "0" ) + '|' +
+        document.querySelector("#txtColor").value + '|' +
         document.querySelector("#codeBackground").value + '|' +
         document.querySelector("#linkColor").value;
     browser.storage.sync.set({
         Colors: Colors,
     });
-    /*     browser.storage.sync.set({
-            txtColor: document.querySelector("#txtColor").value,
-        });
-        browser.storage.sync.set({
-            backgroundColor: document.querySelector("#backgroundColor").value,
-        });
-        browser.storage.sync.set({
-            testColor: document.querySelector("#testColor").value,
-        });
-        browser.storage.sync.set({
-            darkBlueColor: document.querySelector("#darkBlueColor").value,
-        });
-        browser.storage.sync.set({
-            codeBackground: document.querySelector("#codeBackground").value,
-        });
-        browser.storage.sync.set({
-            linkColor: document.querySelector("#linkColor").value,
-        }); */
 }
 
 function restoreOptions() {
     function setCurrentColors(result) {
+        let lVersion; 
+        let lBackgroundColor;
+        let lEnableTextColoring;
+        let lTxtColor;
+        let lCodeBackground;
+        let lLinkColor;
         try {
             let ColorsList = result.Colors.split("|");
-            ltxtColor = ColorsList[0];
-            lbackgroundColor = ColorsList[1];
-            ltestColor = ColorsList[2];
-            ldarkBlueColor = ColorsList[3];
-            lcodeBackground = ColorsList[4];
-            llinkColor = ColorsList[5];
+            lVersion = ColorsList[0];
+            if (gVersion !== lVersion)
+                throw new Error("Version difference -> reset settings.");
+            lBackgroundColor = ColorsList[1];
+            lEnableTextColoring = ColorsList[2];
+            lTxtColor = ColorsList[3];
+            lCodeBackground = ColorsList[4];
+            lLinkColor = ColorsList[5];
         } catch (error) {
-            ltxtColor = "#8F99A5";
-            lbackgroundColor = "#2A2C37";
-            ltestColor = "#1FED18";
-            ldarkBlueColor = "#000022";
-            lcodeBackground = "#1c1c1c";
-            llinkColor = "#687b9a";
+            console.log(error);
+            lVersion = gVersion;
+            lBackgroundColor = "#2A2C37";
+            lEnableTextColoring = "1";
+            lTxtColor = "#8F99A5";
+            lCodeBackground = "#1c1c1c";
+            lLinkColor = "#687b9a";
         }
-        document.querySelector("#txtColor").value = ltxtColor;
-        document.querySelector("#backgroundColor").value = lbackgroundColor;
-        document.querySelector("#testColor").value = ltestColor;
-        document.querySelector("#darkBlueColor").value = ldarkBlueColor;
-        document.querySelector("#codeBackground").value = lcodeBackground;
-        document.querySelector("#linkColor").value = llinkColor;
-        /*         document.querySelector("#txtColor").value = result.txtColor || "#8F99A5";
-                document.querySelector("#backgroundColor").value = result.backgroundColor || "#2A2C37";
-                document.querySelector("#testColor").value = result.testColor || "#1FED18";
-                document.querySelector("#darkBlueColor").value = result.darkBlueColor || "#000022";
-                document.querySelector("#codeBackground").value = result.codeBackground || "#1c1c1c";
-                document.querySelector("#linkColor").value = result.linkColor || "#687b9a";
-         */
-    }
-    function setCurrentChoicetxtColor(result) {
-        document.querySelector("#txtColor").value = result.txtColor || "#8F99A5";
-    }
-    function setCurrentChoicebackgroundColor(result) {
-        document.querySelector("#backgroundColor").value = result.backgroundColor || "#2A2C37";
-    }
-    function setCurrentChoicetestColor(result) {
-        document.querySelector("#testColor").value = result.testColor || "#1FED18";
-    }
-    function setCurrentChoicedarkBlueColor(result) {
-        document.querySelector("#darkBlueColor").value = result.darkBlueColor || "#000022";
-    }
-    function setCurrentChoicecodeBackground(result) {
-        document.querySelector("#codeBackground").value = result.codeBackground || "#1c1c1c";
-    }
-    function setCurrentChoicelinkColor(result) {
-        document.querySelector("#linkColor").value = result.linkColor || "#687b9a";
+        document.querySelector("#version").value = lVersion;
+        document.querySelector("#backgroundColor").value = lBackgroundColor;
+        if (lEnableTextColoring==="1")
+            document.querySelector("#enableTextColoring").checked = true;
+        document.querySelector("#txtColor").value = lTxtColor;
+        document.querySelector("#codeBackground").value = lCodeBackground;
+        document.querySelector("#linkColor").value = lLinkColor;
     }
 
     function onError(error) {
@@ -95,19 +65,6 @@ function restoreOptions() {
 
     let Colors = browser.storage.sync.get("Colors");
     Colors.then(setCurrentColors, onError);
-    /*     let gettingtxtColor = browser.storage.sync.get("txtColor");
-        gettingtxtColor.then(setCurrentChoicetxtColor, onError);
-        let gettingbackgroundColor = browser.storage.sync.get("backgroundColor");
-        gettingbackgroundColor.then(setCurrentChoicebackgroundColor, onError);
-        let gettingtestColor = browser.storage.sync.get("testColor");
-        gettingtestColor.then(setCurrentChoicetestColor, onError);
-        let gettingdarkBlueColor = browser.storage.sync.get("darkBlueColor");
-        gettingdarkBlueColor.then(setCurrentChoicedarkBlueColor, onError);
-        let gettingcodeBackground = browser.storage.sync.get("codeBackground");
-        gettingcodeBackground.then(setCurrentChoicecodeBackground, onError);
-        let gettinglinkColor = browser.storage.sync.get("linkColor");
-        gettinglinkColor.then(setCurrentChoicelinkColor, onError);    
-     */
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
